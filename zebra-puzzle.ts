877d90ee3e2256163b194b5a7aa056f3700700d0
Michael "Z" Goddard <mzgoddard@gmail.com>
2020-02-12 20:20:08 -0500

    its alive

--- zebra-puzzle.ts
+++ zebra-puzzle.ts
@@ -1,6 +1,6 @@
 // house(color, nation, pet, drink, movie genre)
 
-import {Ptr, bind, bound, unbound, value, ref, read, UnboundAddress, BoundAddress, bind2, bindUnbound} from '.';
+import {Ptr, bind, bound, unbound, value, ref, read, UnboundAddress, BoundAddress, bind2, bindUnbound, ask, addr, formatValue, formatAddress} from '.';
 
 enum Color {Red}
 enum Nation {Japan}
@@ -68,73 +68,110 @@ const match = function* <T>(a: Ptr<T>, b: Ptr<T>) {
 }
 
 const exists = function* (v, l: List) {
-    let b = match(v, l[0]);
-    while (yield b) if ((yield true) === false) return;
-    b = match(v, l[1]);
-    while (yield b) if ((yield true) === false) return;
-    b = match(v, l[2]);
-    while (yield b) if ((yield true) === false) return;
-    b = match(v, l[3]);
-    while (yield b) if ((yield true) === false) return;
-    b = match(v, l[4]);
-    while (yield b) if ((yield true) === false) return;
+    // console.log(1, JSON.stringify(l, null, "  "));
+    {
+        const b = match(list(v, addr(), addr(), addr(), addr()), l);
+        while ((yield b)) {if ((yield true) === false) return;}
+    }
+    // console.log(2, JSON.stringify(l, null, "  "));
+    {
+        const b = match(list(addr(), v, addr(), addr(), addr()), l);
+        while ((yield b)) {if ((yield true) === false) return;}
+    }
+    // console.log(3, JSON.stringify(l, null, "  "));
+    {
+    const b = match(list(addr(), addr(), v, addr(), addr()), l);
+    while (yield b) {if ((yield true) === false) return;}
+    }
+    {const b = match(list(addr(), addr(), addr(), v, addr()), l);
+    while (yield b) {if ((yield true) === false) return;}
+    }
+    {
+        const b = match(list(addr(), addr(), addr(), addr(), v), l);
+        while (yield b) {if ((yield true) === false) return;}
+    }
 };
 
 const rightOf = function* (a, b, l) {
-    let r = match(list(b, a, null, null, null), l);
+    let r = match(list(b, a, addr(), addr(), addr()), l);
     while (yield r) if ((yield true) === false) return;
-    r = match(list(null, b, a, null, null), l);
+    r = match(list(addr(), b, a, addr(), addr()), l);
     while (yield r) if ((yield true) === false) return;
-    r = match(list(null, null, b, a, null), l);
+    r = match(list(addr(), addr(), b, a, addr()), l);
     while (yield r) if ((yield true) === false) return;
-    r = match(list(null, null, null, b, a), l);
+    r = match(list(addr(), addr(), addr(), b, a), l);
     while (yield r) if ((yield true) === false) return;
 };
 
 const nextTo = function* (a, b, l) {
-    let r = match(list(b, a, null, null, null), l);
+    let r = match(list(b, a, addr(), addr(), addr()), l);
     while (yield r) if ((yield true) === false) return;
-    r = match(list(null, b, a, null, null), l);
+    r = match(list(addr(), b, a, addr(), addr()), l);
     while (yield r) if ((yield true) === false) return;
-    r = match(list(null, null, b, a, null), l);
+    r = match(list(addr(), addr(), b, a, addr()), l);
     while (yield r) if ((yield true) === false) return;
-    r = match(list(null, null, null, b, a), l);
+    r = match(list(addr(), addr(), addr(), b, a), l);
     while (yield r) if ((yield true) === false) return;
-    r = match(list(a, b, null, null, null), l);
+    r = match(list(a, b, addr(), addr(), addr()), l);
     while (yield r) if ((yield true) === false) return;
-    r = match(list(null, a, b, null, null), l);
+    r = match(list(addr(), a, b, addr(), addr()), l);
     while (yield r) if ((yield true) === false) return;
-    r = match(list(null, null, a, b, null), l);
+    r = match(list(addr(), addr(), a, b, addr()), l);
     while (yield r) if ((yield true) === false) return;
-    r = match(list(null, null, null, a, b), l);
+    r = match(list(addr(), addr(), addr(), a, b), l);
     while (yield r) if ((yield true) === false) return;
 };
 
 const middle = function* (a, l) {
-    const r = match(list(null, null, a, null, null), l);
+    const r = match(list(addr(), addr(), a, addr(), addr()), l);
     while (yield r) if ((yield true) === false) return;
 };
 
 const first = function* (a, l) {
-    const r = match(list(a, null, null, null, null), l);
-    while (yield r) if ((yield true) === false) return;
+    const r = match(list(a, addr(), addr(), addr(), addr()), l);
+    while (yield r) {if ((yield true) === false) return;}
 };
 
 const puzzle = function* (houses) {
     let a, b, c, d, e, f, g, h, i, j, k, l, m, n;
-    a = exists(house('red', 'england', null, null, null), houses);
-    while ((yield a) && (b = exists(house(null, 'spain', 'dog', null, null), houses)))
-    while ((yield b) && (c = exists(house('green', null, null, 'coffee', null), houses)))
-    while ((yield c) && (d = exists(house(null, 'ukraine', null, 'tea', null), houses)))
-    while ((yield d) && (e = rightOf(house('green', null, null, null, null), house('ivory', null, null, null, null), houses)))
-    while ((yield e) && (f = exists(house(null, null, 'snails', null, 'fantasy'), houses)))
-    while ((yield f) && (g = exists(house('yellow', null, null, null, 'sci-fi'), houses)))
-    while ((yield g) && (h = middle(house(null, null, null, 'milk', null), houses)))
-    while ((yield h) && (i = first(house(null, 'norweigh', null, null, null), houses)))
-    while ((yield i) && (j = nextTo(house(null, null, null, null, 'romance'), house(null, null, 'fox', null, null), houses)))
-    while ((yield j) && (k = nextTo(house(null, null, null, null, 'sci-fi'), house(null, null, 'horses', null, null), houses)))
-    while ((yield k) && (l = exists(house(null, null, null, 'orange-juice', 'comedy'), houses)))
-    while ((yield l) && (m = exists(house(null, 'japan', null, null, 'action'), houses)))
-    while ((yield m) && (n = nextTo(house(null, 'norweigh', null, null, null), house('blue', null, null, null, null), houses)))
+    a = exists(house('red', 'england', addr(), addr(), addr()), houses);
+    while ((yield a) && (b = exists(house(addr(), 'spain', 'dog', addr(), addr()), houses)))
+    while ((yield b) && (c = exists(house('green', addr(), addr(), 'coffee', addr()), houses)))
+    while ((yield c) && (d = exists(house(addr(), 'ukraine', addr(), 'tea', addr()), houses)))
+    while ((yield d) && (e = rightOf(house('green', addr(), addr(), addr(), addr()), house('ivory', addr(), addr(), addr(), addr()), houses)))
+    while ((yield e) && (f = exists(house(addr(), addr(), 'snails', addr(), 'fantasy'), houses)))
+    while ((yield f) && (g = exists(house('yellow', addr(), addr(), addr(), 'sci-fi'), houses)))
+    while ((yield g) && (h = middle(house(addr(), addr(), addr(), 'milk', addr()), houses)))
+    while ((yield h) && (i = first(house(addr(), 'norweigh', addr(), addr(), addr()), houses)))
+    while ((yield i) && (j = nextTo(house(addr(), addr(), addr(), addr(), 'romance'), house(addr(), addr(), 'fox', addr(), addr()), houses)))
+    while ((yield j) && (k = nextTo(house(addr(), addr(), addr(), addr(), 'sci-fi'), house(addr(), addr(), 'horses', addr(), addr()), houses)))
+    while ((yield k) && (l = exists(house(addr(), addr(), addr(), 'orange-juice', 'comedy'), houses)))
+    while ((yield l) && (m = exists(house(addr(), 'japan', addr(), addr(), 'action'), houses)))
+    while ((yield m) && (n = nextTo(house(addr(), 'norweigh', addr(), addr(), addr()), house('blue', addr(), addr(), addr(), addr()), houses)))
     while (yield n) if ((yield true) as boolean === false) return;
 };
+
+(async () => {
+    const h = addr();
+    if (ask(puzzle(h))) {
+        console.log(JSON.stringify(formatAddress(h), null, "  "));
+    } else {
+        console.log('no answer');
+    }
+})();
+
+// (() => {
+//     const answer = addr();
+//     function* puzzle(answer) {
+//         let a, b, c;
+//         a = exists(['a', addr(), addr()], answer);
+//         while ((yield a) && ((console.log('a', JSON.stringify((answer)))), b = first([addr(), 'c', addr()], answer))) {
+//             while ((yield b) && ((console.log('b', JSON.stringify((answer)))), c = first(['b', addr(), addr()], answer))) {
+//                 while (yield c) {
+//                     if ((console.log('c', JSON.stringify((answer))), yield true) === false) return;
+//                 }
+//             }
+//         }
+//     };
+//     console.log(ask(puzzle(answer)), JSON.stringify(answer, null, "  "));
+// })();
